567c43d35622852a8401fb61d42c5556a4c93bcf
jewelcsebu <jewelchowdhurybucse@gmail.com>
2022-01-20 14:35:12 +0600

    fixed some seller service issues

--- src/main/java/com/jewelcse045/sellerservice/controller/VoucherController.java
+++ src/main/java/com/jewelcse045/sellerservice/controller/VoucherController.java
@@ -5,27 +5,21 @@ import com.jewelcse045.sellerservice.dto.VoucherDto;
 import com.jewelcse045.sellerservice.model.Voucher;
 import com.jewelcse045.sellerservice.service.VoucherService;
 import com.jewelcse045.sellerservice.util.JsonResponseEntityModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/seller-service")
 public class VoucherController {
 
 
-    JsonResponseEntityModel responseModel = new JsonResponseEntityModel();
+    private final VoucherService voucherService;
 
-
-    private VoucherService voucherService;
-
-    @Autowired
-    public VoucherController(VoucherService voucherService){
-        this.voucherService = voucherService;
-    }
 
     @PostMapping("/create/voucher")
     public ResponseEntity<Voucher> createVoucher(@RequestBody VoucherDto voucherDto){
